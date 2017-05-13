f3e9db068e15701e4ccec45bea16d2ea34f2de9b
Joseph Mechali <mechali.joseph@gmail.com>
2017-05-13 15:51:10 -0400

    enleverMot et chercherMot
    A debugger

--- Devoir1/Devoir1/Dictionary.cpp
+++ Devoir1/Devoir1/Dictionary.cpp
@@ -26,7 +26,51 @@ void Dictionary::ajouterMot(std::string s) {
 }
 
 void Dictionary::enleverMot(std::string s) {
+	int tailleMot = s.size;
 
+	if (!chercheMot(s))
+		return;
+
+	enleverMotRec(s, *racine);
+
+	return;
+}
+
+//precondition : le mot est dans l arbre
+bool Dictionary::enleverMotRec(std::string s, Noeud<char>& arbre){
+
+	//comme le mot est present, on y est
+	if (s.size == 1 && s[0] == arbre.data) {
+		arbre.endWord = false;
+		return true;
+	}
+
+	int tailleMot = s.size;
+	std::string sousChaine = s.substr(1, tailleMot);
+
+	//si la lettre suivante appartient au mot
+	if (enleverMotRec(sousChaine, *arbre.nextLetter)) {
+		//si la lettre n est plus utilisee, la retirer
+		if (arbre.nextLetter->nextLetter == 0 && !arbre.nextLetter->endWord) {
+
+			//delete *(arbre.nextLetter);
+			arbre.nextLetter = arbre.nextLetter->alternative;
+		}
+		//on doit etre modifie par le predecesseur, car on appartient au mot
+		return true;
+	}
+	//si la lettre alternative appartient au mot
+	if (enleverMotRec(s, *arbre.alternative)) {
+		if (arbre.alternative->nextLetter == 0 && !arbre.alternative->endWord) {
+
+			//delete *(arbre.alternative);
+			arbre.alternative = arbre.alternative->alternative;
+		}
+		//on n appartient pas au mot, on ne doit pas etre modifie
+		return false;
+	}
+	//qu on appartienne au mot ou non, nos fils n ont pas ete modifies donc on ne doit pas etre modifie
+	return false;
 }
 
 void Dictionary::afficherDict() {
@@ -34,7 +78,32 @@ void Dictionary::afficherDict() {
 }
 
 bool Dictionary::chercheMot(std::string s) {
-	return true;
+	
+	return chercherMotDansArbre(s,*racine);
+}
+
+bool Dictionary::chercherMotDansArbre(std::string s, Noeud<char>& arbre) {
+	int tailleMot = s.size;
+
+	if (tailleMot == 0)
+		return false;
+
+	if (tailleMot == 1 && s[0] == arbre.data && arbre.endWord) {//&& arbre.finDeMot)
+		return true;
+	}
+
+	if (arbre.alternative == 0 && (s[0] != arbre.data)) {
+		return false;
+	}
+
+	std::string sousChaine = s.substr(1, tailleMot);
+	if (s[0] == arbre.data) {		
+		return chercherMotDansArbre(sousChaine, *(arbre.nextLetter));	
+	}
+	else
+	{
+		return chercherMotDansArbre(s, *(arbre.alternative));
+	}
 }
 
 
--- Devoir1/Devoir1/Dictionary.h
+++ Devoir1/Devoir1/Dictionary.h
@@ -2,27 +2,32 @@
 
 class Dictionary
 {
-public:
-	Dictionary(std::string filename = "");
-
-	void ajouterMot(std::string s);
-	void enleverMot(std::string s);
-	void afficherDict();
-	bool chercheMot(std::string s);
-
-	//void afficherCharDict();
-
-	~Dictionary();
-
 private:
 	template<typename E>
 	class Noeud {
 	public:
 		E data;
+		bool endWord;
 		Noeud *nextLetter;
 		Noeud *alternative;
 		Noeud(const E& d) : nextLetter(0), data(d), alternative(0) { }
 	};
 	Noeud<char>* racine;
+
+public:
+	Dictionary(std::string filename = "");
+
+	void ajouterMot(std::string s);
+	void enleverMot(std::string s);
+	bool enleverMotRec(std::string, Noeud<char>&);
+	void afficherDict();
+	bool chercheMot(std::string s);
+	bool chercherMotDansArbre(std::string s, Noeud<char>& arbre);
+
+	//void afficherCharDict();
+
+	~Dictionary();
+
+
 };
 
