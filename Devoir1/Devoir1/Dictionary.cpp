d7977fbffd24734c8ea09dd436659df5988fafa8
Loïc Leinot <cioltoniel@hotmail.fr>
2017-05-13 15:50:30 -0400

    Ajout de mots et affichage dictionnaire

--- Devoir1/Devoir1/Dictionary.cpp
+++ Devoir1/Devoir1/Dictionary.cpp
@@ -6,6 +6,8 @@ using namespace std;
 
 Dictionary::Dictionary(std::string filename)
 {
+	racine = new Noeud<char>('/');
+
 	//arbre = new Arbre<char>();
 	ifstream myReadFile;
 	myReadFile.open(filename);
@@ -14,23 +16,60 @@ Dictionary::Dictionary(std::string filename)
 		while (!myReadFile.eof()) {
 			myReadFile >> output;
 			ajouterMot(output);
-			cout << output << endl;
 		}
 	}
 	myReadFile.close();
 }
 
 void Dictionary::ajouterMot(std::string s) {
-	Noeud<char>* a;
-	cout << s.size() << endl;
+	Noeud<char>* current(racine);
+	std::string remainingS(s);
+	int i(0), sSize(s.size());
+
+	while (i < sSize) {
+
+		while (current->data != s[i] && current->alternative != 0) {
+			current = current->alternative;
+		}
+		if (current->data != s[i]) {
+			Noeud<char>* newNoeud = new Noeud<char>(s[i]);
+			current->alternative = newNoeud;
+			current = current->alternative;
+			createEndWord(s, current, i);
+			break;
+		} else if (current->data == s[i]) {
+			if (current->nextLetter == 0) {
+				createEndWord(s, current, i);
+				break;
+			}
+			current = current->nextLetter;
+		} else {
+			cout << "Unknown error" << endl;
+		}
+		i++;
+	}
 }
+
+void Dictionary::createEndWord(std::string s, Noeud<char>* n, int index) {
+	int i(index);
+	Noeud<char>* current = n;
+	i++;
+	while (i < s.size()) {
+		Noeud<char>* newNoeud = new Noeud<char>(s[i]);
+		current->nextLetter = newNoeud;
+		current = current->nextLetter;
+		i++;
+	}
+	current->endWord = true;
+}
+
 
 void Dictionary::enleverMot(std::string s) {
 
 }
 
 void Dictionary::afficherDict() {
-
+	afficherCharDict("", racine);
 }
 
 bool Dictionary::chercheMot(std::string s) {
@@ -41,4 +80,17 @@ bool Dictionary::chercheMot(std::string s) {
 Dictionary::~Dictionary()
 {
 	//delete(arbre);
+}
+
+void Dictionary::afficherCharDict(std::string s, Noeud<char>* current) {
+	if (current->endWord) {
+		cout << s + current->data << endl;
+	}
+
+	if (current->nextLetter !=0) {
+		afficherCharDict(s + current->data, current->nextLetter);
+	}
+	if (current->alternative != 0) {
+		afficherCharDict(s, current->alternative);
+	}
 }
--- Devoir1/Devoir1/Dictionary.h
+++ Devoir1/Devoir1/Dictionary.h
@@ -2,6 +2,22 @@
 
 class Dictionary
 {
+private:
+	template<typename E>
+	class Noeud {
+	public:
+		E data;
+		bool endWord;
+		Noeud *nextLetter;
+		Noeud *alternative;
+		Noeud(const E& d) : nextLetter(0), data(d), alternative(0), endWord(false) { }
+	};
+	Noeud<char>* racine;
+
+
+	//Needed Functions
+	void createEndWord(std::string s, Noeud<char>* n, int index);
+
 public:
 	Dictionary(std::string filename = "");
 
@@ -10,19 +26,9 @@ public:
 	void afficherDict();
 	bool chercheMot(std::string s);
 
-	//void afficherCharDict();
+	void afficherCharDict(std::string s, Noeud<char>* current);
 
 	~Dictionary();
 
-private:
-	template<typename E>
-	class Noeud {
-	public:
-		E data;
-		Noeud *nextLetter;
-		Noeud *alternative;
-		Noeud(const E& d) : nextLetter(0), data(d), alternative(0) { }
-	};
-	Noeud<char>* racine;
 };
 
