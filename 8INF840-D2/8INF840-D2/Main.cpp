816f816b2cd7c9c5c6c47da6cab62357cba04689
Loïc Leinot <cioltoniel@hotmail.fr>
2017-06-06 11:51:59 -0400

    Init D2

---
+++ 8INF840-D2/8INF840-D2/Main.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+void KdTree() {
+
+}
+
+void PageRank() {
+
+}
+
+void Graph() {
+
+}
+
+void menu() {
+	int choice = -1;
+
+	while (choice != 0) {
+		cout << "------------------------" << endl;
+		cout << "Choix de l'exercice :" << endl << endl;
+		cout << "1 - KdTree :" << endl;
+		cout << "2 - PageRank" << endl;
+		cout << "3 - Graph" << endl;
+		cout << "0 - EXIT" << endl;
+
+		cin >> choice;
+		cin.clear();
+		cin.ignore(1000, '\n');
+		cout << endl;
+
+		switch (choice) {
+		case 0:
+			break;
+		case 1:
+			KdTree();
+			break;
+		case 2:
+			PageRank();
+			break;
+		case 3:
+			Graph();
+			break;
+		default:
+			break;
+		}
+	}
+}
+
+
+
+int main() {
+
+	menu();
+
+	system("PAUSE");
+	return 0;
+}
