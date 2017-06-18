73fd259b0becec3840eb9d31e6e6a790cce2e226
Guixter <g.singland@gmail.com>
2017-06-18 15:57:52 -0400

    fixing the merge

--- 8INF840-D2/8INF840-D2/Main.cpp
+++ 8INF840-D2/8INF840-D2/Main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <string>
+#include "SpacePartitioning.h"
 #include "PageRanker.h"
+#include "Zombie.h"
 using namespace std;
 
 void KdTree() {
@@ -95,41 +97,6 @@ void menu() {
 		}
 	}
 }
-
-void menu() {
-	int choice = -1;
-
-	while (choice != 0) {
-		cout << "------------------------" << endl;
-		cout << "Choix de l'exercice :" << endl << endl;
-		cout << "1 - KdTree :" << endl;
-		cout << "2 - PageRank" << endl;
-		cout << "3 - Graph" << endl;
-		cout << "0 - EXIT" << endl;
-
-		cin >> choice;
-		cin.clear();
-		cin.ignore(1000, '\n');
-		cout << endl;
-
-		switch (choice) {
-		case 0:
-			break;
-		case 1:
-			KdTree();
-			break;
-		case 2:
-			PageRank();
-			break;
-		case 3:
-			Graph();
-			break;
-		default:
-			break;
-		}
-	}
-}
-
 
 int main() {
 
